043af440abb9d2bd09692b514ceadd3829003cce
pihappy <43689732+pihappy@users.noreply.github.com>
2018-11-17 15:10:06 -0200

    Initial commit

---
+++ ProjetoIntegrador vs 5.0/src/Controller/CadastrarProdutoController.java
@@ -0,0 +1,38 @@
+package Controller;
+
+import DAO.CadastrarProdutoDAO;
+import Model.CadastrarProduto;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author gabriela.vsmarques
+ */
+public class CadastrarProdutoController {
+
+    public static boolean salvar(int pcodigoProduto, String pdescricaoProduto, int pquantidadeProduto, double pvalorUni, String pcategoriaProduto) {
+       // CadastrarProduto produto = new CadastrarProduto(int codigoProduto, String descricaoProduto, int quantidadeProduto, double valorUni, String categoriaProduto);
+    //return CadastrarProdutoDAO.salvar(produto);
+
+    }
+
+    public static ArrayList<String[]> getCadastrarProduto() {
+
+        ArrayList<CadastrarProduto> produto = CadastrarProdutoDAO.getCadastrarProduto();
+        ArrayList<String[]> listaCadastroProduto = new ArrayList<>();
+
+        for (int i = 0; i < produto.size(); i++) {
+            listaCadastroProduto.add(new String[]{String.valueOf(produto.get(i).getCodigoProduto()),
+                produto.get(i).getDescricaoProduto(),
+                String.valueOf(produto.get(i).getQuantidadeProduto()),
+                String.valueOf(produto.get(i).getValorUni()),
+                String.valueOf(produto.get(i).getCategoriaProduto())});
+
+        }
+
+        return listaCadastroProduto;
+
+    }
+
+}
---
+++ ProjetoIntegrador vs 5.0/src/DAO/CadastrarProdutoDAO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.CadastrarProduto;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
+
+/**
+ *
+ * @author gabriela.vsmarques
+ */
+public class CadastrarProdutoDAO {
+    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String SERVIDOR = "localhost";     
+    private static final String BASEDADOS = "pihappy"; 
+    private static final String LOGIN = "root";            
+    private static final String SENHA = "adminadmin";
+    private static String url = "";
+    private static Connection conexao;
+    
+    //public static boolean salvar(CadastrarProduto produto){
+    
+     //boolean retorno = false;
+    
+       
+    
+    
+    
+      
+}
